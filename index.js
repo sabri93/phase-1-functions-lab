9e873d1b9ca40803ba8a31fc057e05872b23b2b8
sabrina <sabrinadjebbar93@gmail.com>
2022-03-23 17:54:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+
+let start = x;
+let destination = y;
+
+function distanceFromHqInBlocks(y){
+    if (y>42){
+        return y-42
+    }
+   else {return 42 - y}
+}
+
+function distanceFromHqInFeet(y){
+   if (y>42){
+       return (y-42)*264
+   }
+   else {
+       return(42-y)*264
+   }
+
+}
+
+   function distanceTravelledInFeet(x,y){
+    if (y>x){
+        return (y-x)*264
+    } else{
+        return (x-y)*264
+    }
+}
+ 
+ 
+function calculatesFarePrice(x,y){
+   
+     let distance1 = (y-x)*264;
+    let distance2 = (x-y)*264;
+    let price;
+  
+   if (distance1||distance2 <=400){
+       return price = 0;
+
+}  else if(400< distance1 || distance2 <2000){
+       return price = (distance1 ||distance2-400) * 0.02;  
+
+}   else if ( 2000< distance1 || distance2 <2500){
+     return price = 25 
+   
+}   else if(distance1 || distance2 >2500){
+    return 'cannot travel that far'
+}
+ 
+
+}
